437df43eff1f3d90faf8304740ae7bbd97f527c3
Emma-Louise Upton <emma-louise.upton@hmrcaspire.com>
2017-02-02 15:18:55 +0000

    Updated DESService

--- app/services/DESService.scala
+++ app/services/DESService.scala
@@ -18,7 +18,7 @@ package services
 
 import javax.inject.{Inject, Singleton}
 import connectors._
-import models.{RegisterModel, SubscribeModel, SubscriptionRequest}
+import models.{FullDetails, RegisterModel, SubscribeModel, SubscriptionRequest}
 import play.api.libs.json.Json
 import uk.gov.hmrc.domain.Nino
 import uk.gov.hmrc.play.http.HeaderCarrier
@@ -29,22 +29,22 @@ import scala.concurrent.Future
 @Singleton
 class DESService @Inject()(dESConnector: DESConnector, taxEnrolmentsConnector: TaxEnrolmentsConnector) {
 
+  def fetchSap(response: DesResponse) = {
+    response match {
+      case SuccessDesResponse(data) => {
+        Future.successful(data.as[String])}
+      case InvalidDesRequest(message) => Future.failed(new Exception(message))
+    }
+  }
+
+  def fetchCGTReference(response: DesResponse) = {
+    response match {
+      case SuccessDesResponse(data) => Future.successful(data.as[String])
+      case InvalidDesRequest(message) => Future.failed(new Exception(message))
+    }
+  }
+
   def subscribeUser(nino: String)(implicit hc: HeaderCarrier): Future[String] = {
-
-    def fetchSap(response: DesResponse) = {
-      response match {
-        case SuccessDesResponse(data) => {
-          Future.successful(data.as[String])}
-        case InvalidDesRequest(message) => Future.failed(new Exception(message))
-      }
-    }
-
-    def fetchCGTReference(response: DesResponse) = {
-      response match {
-        case SuccessDesResponse(data) => Future.successful(data.as[String])
-        case InvalidDesRequest(message) => Future.failed(new Exception(message))
-      }
-    }
 
     for {
       bpResponse <- dESConnector.obtainBp(RegisterModel(Nino(nino)))
@@ -53,6 +53,17 @@ class DESService @Inject()(dESConnector: DESConnector, taxEnrolmentsConnector: T
       cgtRef <- fetchCGTReference(subscribeResponse)
       enrolmentIssuerRequest <- taxEnrolmentsConnector.getIssuerResponse(cgtRef, Json.toJson(cgtRef))
       enrolmentSubscriberRequest <- taxEnrolmentsConnector.getSubscriberResponse(cgtRef, Json.toJson(cgtRef))
+    } yield cgtRef
+  }
+
+  def subscribeGhostUser(fullDetails: FullDetails)(implicit hc: HeaderCarrier): Future[String] = {
+    for {
+      bpResponse <- dESConnector.obtainBpGhost(fullDetails)
+      sap <- fetchSap(bpResponse)
+      subscribeResponse <- dESConnector.subscribe(SubscribeModel(sap))
+      cgtRef <- fetchCGTReference(subscribeResponse)
+      enrolmentIssuerRequest <- taxEnrolmentsConnector.getIssuerResponse(cgtRef, Json.toJson(cgtRef))
+      enrolmentsSubscriberRequest <- taxEnrolmentsConnector.getSubscriberResponse(cgtRef, Json.toJson(cgtRef))
     } yield cgtRef
   }
 }
